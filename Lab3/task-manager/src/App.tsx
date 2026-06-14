5534b9a72aaae0858edd9ac241d6da570be40cd7
S-I-practica-STIP <wanyuchas@gmail.com>
2026-06-14 12:49:39 +0300

    Lab 3: Add TaskCard component

---
+++ Lab3/task-manager/src/App.tsx
@@ -0,0 +1,26 @@
+import TaskCard from "./components/TaskCard/TaskCard";
+import type { Task } from "./types/task";
+
+
+const mockTask: Task = {
+  id: "1",
+  title: "Тестова задача",
+  description: "Перевірка відображення картки",
+  status: "todo",
+  priority: "high",
+  createdAt: new Date(),
+};
+
+
+function App() {
+  return (
+    <TaskCard
+      task={mockTask}
+      onDelete={(id) => console.log("delete", id)}
+      onStatusChange={(id, status) => console.log("status", id, status)}
+    />
+  );
+}
+
+
+export default App;
---
+++ Lab3/task-manager/src/components/TaskCard/TaskCard.tsx
@@ -0,0 +1,68 @@
+﻿import clsx from 'clsx';
+import type { Task, TaskStatus } from '../../types/task';
+import styles from './TaskCard.module.css';
+
+
+interface TaskCardProps 
+{
+  task: Task;
+  onDelete: (id: string) => void;
+  onStatusChange: (id: string, status: TaskStatus) => void;
+}
+
+
+export default function TaskCard(props: TaskCardProps) 
+{
+    //деструктуризація 
+    const { task, onDelete, onStatusChange } = props;
+
+
+    //підготовлюємо дату
+    const formattedDate = task.createdAt.toLocaleDateString('uk-UA');
+
+
+    //обробник події кліку на кнопку видалення. визивае колбек 
+    const handleDelete = () => {
+        onDelete(task.id);
+    };
+    //обробник події зміни статусу. визивае колбек
+    const handleStatusChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        onStatusChange(task.id, e.target.value as TaskStatus);
+    };
+
+
+    //повертаємо результат
+    return (
+        //встановлення класу в залежності від пріоритету
+        <div className={clsx(styles.card, 
+        {
+            [styles.cardLow]: task.priority === 'low',
+            [styles.cardMedium]: task.priority === 'medium',
+            [styles.cardHigh]: task.priority === 'high',
+        })}>
+            {/* виводимо заголовок */}
+            <h3 className={styles.title}>{task.title}</h3>
+            {/*якщо task.description є, тоді малюємо description*/}
+            {task.description && <p className={styles.description}>{task.description}</p>}
+            
+            {/*виводимо дані про дату і пріоритет */}
+            <div className={styles.meta}>
+                <span>Дата: {formattedDate}</span>
+                <span>Пріоритет: {task.priority}</span>
+            </div>
+            {/*елементи дій*/}
+            <div className={styles.actions}>
+                {/*створення випадаючого списку*/}
+                <select value={task.status} onChange={handleStatusChange}>
+                    <option value="todo">Треба зробити</option>
+                    <option value="in-progress">В роботі</option>
+                    <option value="done">Готово</option>
+                </select>
+                {/*кнопка видалення*/}
+                <button onClick={handleDelete} className={styles.deleteBtn}>
+                    Видалити
+                </button>
+            </div>
+        </div>
+    );
+}
